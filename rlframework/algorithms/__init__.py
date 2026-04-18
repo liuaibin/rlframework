3640ca0b38e6b2de9b3ef72f6fd7d191827e4561
hairbeauty <liuaibin163@163.com>
2026-04-18 17:43:35 +0800

    fix: del supervised bc mode

--- rlframework/algorithms/__init__.py
+++ rlframework/algorithms/__init__.py
@@ -1,19 +1,7 @@
 from rlframework.algorithms.ppo import CustomPPO
 from rlframework.algorithms.sac import CustomSAC
-from rlframework.algorithms.supervised import (
-    CustomBC,
-    CustomBCConfig,
-    CustomMARWIL,
-    CustomMARWILConfig,
-    SupervisedAlgorithmMixin,
-)
 
 __all__ = [
     "CustomPPO",
     "CustomSAC",
-    "CustomBC",
-    "CustomBCConfig",
-    "CustomMARWIL",
-    "CustomMARWILConfig",
-    "SupervisedAlgorithmMixin",
 ]
